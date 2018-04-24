f63575a998d9815a6300e0877c7408682acb703d
Samuel Yeomans <samuelyeomans@gmail.com>
2018-04-24 19:12:52 -0400

    Tested cron

--- cron.py
+++ cron.py
@@ -1,13 +1,10 @@
 from urlFunctions import postUrl
 
-def cron(creds):
+def runCron(creds):
 	"""
 	Runs cron.
 
 	creds: a dictionary of user credentials formatted as: {'x-api-user': 'your_user_id', 'x-api-key': 'your_api_key'}
 	"""
 	url = "https://habitica.com/api/v3/cron"
-	# payload = {'challenge': {'groupId': groupId, 'name': name, 'shortName': shortName, 'summary': summary, 'description': description, 'prize': prize}, 'official': official}
-	# payload = {}
-	# return(postUrl(url, creds, payload))
 	return(postUrl(url, creds))
