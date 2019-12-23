31cc029b18a2a0998fec515db6b8c91248221cbe
syeomans <samuelyeomans@gmail.com>
2019-12-23 00:35:57 -0500

    Added documentation to status.py.

--- status.py
+++ status.py
@@ -1,11 +1,15 @@
+"""Standalone function to get Habitica's API status."""
+
 from Habotica.urlFunctions import getUrl
 
 def getAPIStatus():
-	"""
-	Get Habitica's API status.
+	"""Get Habitica's API status.
 
-	Returns -- 
-		status: string. 'up' if everything is ok.
+	Args:
+		No arguments.
+
+	Returns:
+		Status string: 'up' if everything is ok.
 	"""
 	url = "https://habitica.com/api/v3/status"
 	return(getUrl(url))
